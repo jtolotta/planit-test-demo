2cb119ffd5946421eb81f6d00a31c511def43daa
Jason Tolotta <jtolotta@gmail.com>
2023-02-04 00:57:15 +1030

    feat(navigate): add navigate services test

--- cypress/e2e/planit/navigation/navigate-services.cy.js
+++ cypress/e2e/planit/navigation/navigate-services.cy.js
@@ -1,15 +1,32 @@
 describe('Navigate Services', () => {
-  before(() => {
-
-  });
-
+  // Navigate using end-user interactions
+  // using the top-menu
   it('View Quality Engineering', () => {
+    cy.visit('/');
+    cy.get('span:contains("SOLUTIONS")')
+      .parent()
+      .should('exist')
+      .invoke('mouseover')
+      .then(() => {
+        cy.get(
+          'div#MegaMenuServicesId td > a[href="/au/services/quality-engineering"]'
+        ).click({ force: true });
+        cy.get('h1:contains("Quality Engineering")').should('exist');
+      });
   });
 
+  // Direct URL navigation
   it('View Testing & QA', () => {
+    cy.visit('/au/services/testing-qa').then(() => {
+      cy.get('h1:contains("Testing & QA")').should('exist');
+    });
   });
 
+  // Bypass drop-down menu and directly invoke
+  // the click event from the DOM element
   it('View Test Automation', () => {
+    cy.visit('/');
+    cy.get('a:contains("Test Automation")').first().click({ force: true });
+    cy.get('h1:contains("Test Automation")').should('exist');
   });
 });
-

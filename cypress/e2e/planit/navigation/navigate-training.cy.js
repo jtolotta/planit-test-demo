2b3b7b7cb226391ad02d88fb0710dd58b7128a30
Jason Tolotta <jtolotta@gmail.com>
2023-02-04 01:30:57 +1030

    feat(navigate): add navigate training test

--- cypress/e2e/planit/navigation/navigate-training.cy.js
+++ cypress/e2e/planit/navigation/navigate-training.cy.js
@@ -1,15 +1,20 @@
 describe('Navigate Training', () => {
-  before(() => {
-
-  });
-
-  it('ISTQB Foundation', () => {
-  });
-
-  it('ISTQB Advanced Test Analyst', () => {
-  });
-
-  it('ISTQB Advanced Test Manager', () => {
+  // Navigate to the ISTQB Foundation training page
+  // and check the service menu scrolls correctly
+  // using a custom command
+  it('View ISTQB Foundation', () => {
+    cy.visit('/au/training/istqb-foundation-certificate');
+    cy.get('h1:contains("ISTQB Foundation Certificate")').should('exist');
+    cy.get('a:contains("Overview")').click();
+    cy.get(
+      'h2:contains("Verify your testing skills with this essential accreditation!")'
+    ).should('be.inViewport');
+    cy.get('h2:contains("ISTQB Foundation Exam")').should('not.be.inViewport');
+    cy.get('a:contains("Exam Details")')
+      .click()
+      .wait(2000)
+      .then(() => {
+        cy.get('h2:contains("ISTQB Foundation Exam")').should('be.inViewport');
+      });
   });
 });
-

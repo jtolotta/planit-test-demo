454d2ca63fc080e23d9fd107c5dc7043a8426dcf
Jason Tolotta <jtolotta@gmail.com>
2023-02-04 01:55:43 +1030

    feat(search): add search single result test

--- cypress/e2e/planit/search/search-single-result.cy.js
+++ cypress/e2e/planit/search/search-single-result.cy.js
@@ -1,8 +1,21 @@
 describe('Search Single Result', () => {
   before(() => {
-
+    cy.visit('/');
   });
 
+  // Use search criteria that returns only a single result
   it('Search for The Rise of Autonomous Vehicles', () => {
+    // Dismiss the overlay if the site refreshes
+    cy.get('body').click({ force: true });
+    cy.get('div[onclick="SearchBoxClick();"]').click();
+    cy.get('div#dp-MegaSearch')
+      .invoke('mouseout')
+      .then(() => {
+        cy.get('div[class="searchBox"] > input')
+          .first()
+          .type('The Rise of Autonomous Vehicles{enter}', { force: true });
+        cy.get('a:contains("The Rise of Autonomous Vehicles")').should('exist');
+        cy.get('div:contains("No more result were found.")').should('exist');
+      });
   });
 });

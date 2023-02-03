05770eb8456a2ff9d836b4d13d425b58c7448b64
Jason Tolotta <jtolotta@gmail.com>
2023-02-04 02:15:22 +1030

    feat(search): add search multiple results test

--- cypress/e2e/planit/search/search-multiple-results.cy.js
+++ cypress/e2e/planit/search/search-multiple-results.cy.js
@@ -1,9 +1,44 @@
 describe('Search Multiple Results', () => {
   before(() => {
-
+    cy.visit('/');
   });
 
+  // Use a search term that returns more than one result
+  // Load all results and check total
   it('Search for Regulatory Compliance', () => {
+    // Dismiss the overlay if the site refreshes
+    cy.get('body').click({ force: true });
+    cy.get('div[onclick="SearchBoxClick();"]').click();
+    cy.get('div#dp-MegaSearch')
+      .invoke('mouseout')
+      .then(() => {
+        cy.get('div[class="searchBox"] > input')
+          .first()
+          .type('Regulatory Compliance{enter}', { force: true });
+        // At least one search result contains the search term
+        cy.get('a:contains("Regulatory Compliance")').should('exist');
+
+        cy.get('span[class*="searchLoadMoreInfo"]')
+          .invoke('text')
+          .then((text) => {
+            const resultCount = parseInt(
+              text.replace('Results Found.', '').trim()
+            );
+
+            // Determine the number of times to click
+            // the load more button to view all results
+            let loadCount = Math.floor(resultCount / 5);
+
+            for (let i = 0; i < loadCount; i += 1) {
+              cy.get('a:contains("Load More")').should('exist').click();
+            }
+
+            // Compare the actual total results with
+            // the expected result value
+            cy.get('ul#searchResult')
+              .find('li')
+              .should('have.length', resultCount);
+          });
+      });
   });
 });
-

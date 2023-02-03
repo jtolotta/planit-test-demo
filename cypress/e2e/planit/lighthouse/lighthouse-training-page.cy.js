0f6b2961739578484ca981d1c5504cc51b1dfe28
Jason Tolotta <jtolotta@gmail.com>
2023-02-04 02:45:45 +1030

    feat(lighthouse): add lighthouse tests

--- cypress/e2e/planit/lighthouse/lighthouse-training-page.cy.js
+++ cypress/e2e/planit/lighthouse/lighthouse-training-page.cy.js
@@ -1,9 +1,29 @@
 describe('Lighthouse Training Page', () => {
-  before(() => {
-
-  });
-
   it('Run Training Page Report', () => {
+    cy.visit('/au/training');
+
+    const thresholds = {
+      performance: 50,
+      accessibility: 50,
+      'best-practices': 70,
+      seo: 70,
+      'first-contentful-paint': 3000,
+      'largest-contentful-paint': 5000,
+      'cumulative-layout-shift': 0.1,
+      'total-blocking-time': 500,
+    };
+
+    const options = {
+      formFactor: 'desktop',
+      screenEmulation: {
+        mobile: false,
+        disable: false,
+        deviceScaleRatio: 1,
+        height: 900,
+        width: 1600,
+      },
+    };
+
+    cy.lighthouse(thresholds, options);
   });
 });
-

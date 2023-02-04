19adfa753ca5753c5446b10220c4706d679516a9
Jason Tolotta <jtolotta@gmail.com>
2023-02-04 22:27:30 +1030

    feat(contact): add contact form invalid test

---
+++ cypress/e2e/planit/contact/contact-form-invalid.cy.js
@@ -0,0 +1,20 @@
+describe('Contact Form Invalid', () => {
+  beforeEach(() => {
+    cy.visit('/au/contact');
+  });
+
+  it('Check for Empty Contact Field Submission', () => {
+    cy.get('input[type="submit"][value="Submit"]').click();
+    cy.get('span:contains("Please enter your first name")').should(
+      'be.visible'
+    );
+    cy.get('span:contains("Please enter your last name")').should('be.visible');
+    cy.get('span:contains("Please enter an Organisation Name")').should(
+      'be.visible'
+    );
+    cy.get('span:contains("Please enter an email address")').should(
+      'be.visible'
+    );
+    cy.get('span:contains("Please enter a phone number")').should('be.visible');
+  });
+});
